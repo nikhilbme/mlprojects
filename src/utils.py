e8fad59f9b54a007c5f360a9be5669f11049b71f
Nikhil Chettipally <chnikhilbme@gmail.com>
2023-08-09 23:04:18 -0400

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,19 @@ import dill
 
 from src.exception import CustomException
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+
+from src.logger import logging
+
+
 def save_object(file_path, obj):
     try: 
         dir_path = os.path.dirname(file_path)
@@ -13,6 +26,37 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            #para=param[list(models.keys())[i]]
+
+            #gs = GridSearchCV(model,para,cv=3)
+            #gs.fit(X_train,y_train)
+
+            #model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
